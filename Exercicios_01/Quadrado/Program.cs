8811efcc6026c9dc73c2e2d4d30e026387f4df54
Vinicius Tengaten <vinitengaten@gmail.com>
2024-02-20 00:09:37 -0300

    first commit

---
+++ Exercicios_01/Quadrado/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program {
+    private static void Main(string[] args) {
+        int lado, area, perimetro;
+
+        Console.Write("Lado: ");
+        lado = Convert.ToInt32(Console.ReadLine());
+        
+        area = lado * lado;
+        perimetro = lado * 4;
+
+        Console.WriteLine("Área: {0}, Perímetro: {1}", area, perimetro);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ELP 2
+Sugestões de códigos para resolver os execícios propostos nas aulas de ELP 2
